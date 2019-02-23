6dc30ece84edd378e114dc1f4df16f4acd4f266f
Alexander Badewitz <alba@DAKOTAa.badewitz@adb-solutions.de>
2019-02-23 18:27:52 +0100

    Business-Schicht fast fertig.

--- Alex/CsvViewer/CsvViewer.Business/Interaktionen.cs
+++ Alex/CsvViewer/CsvViewer.Business/Interaktionen.cs
@@ -10,40 +10,72 @@ namespace CsvViewer.Business
 {
     public class Interaktionen
     {
+        SeitenBereitsteller _seitenBereitsteller = new SeitenBereitsteller();
+
         public Interaktionen()
         {
-
+            _seitenBereitsteller = new SeitenBereitsteller();
         }
 
         public List<CsvDatensatz> Start(string[] args)
         {
             string pfad = new ArgumentVerarbeiter().Lese_Eingabeparameter(args);
             List<string> zeilen = new DateiBereitsteller().Lese_Dateiinhalt(pfad);
-            List<CsvDatensatz> datensaetze = new CsvParser().Zerlege_Csv_in_CsvDatensaetze(zeilen).ToList();
+            List<CsvDatensatz> alleDatensaetze = new CsvParser().Zerlege_Csv_in_CsvDatensaetze(zeilen).ToList();
+            Status.Instanz.CsvDatensaetze.Setze(alleDatensaetze);
+            List<CsvDatensatz> datensaetzeErsteSeite = Erste_Seite();
 
-            Status.Instanz.CsvDatensaetze.Setze(datensaetze);
-
-            return Erste_Seite();
+            return datensaetzeErsteSeite;
         }
 
         public List<CsvDatensatz> Erste_Seite()
         {
-            throw new NotImplementedException();
+            List<CsvDatensatz> alleDatensaetze = Status.Instanz.CsvDatensaetze.Lade();
+            int seitenlaenge = Status.Instanz.Seitenlaenge.Lade();
+            int seitenNummer = _seitenBereitsteller.Ermittle_Seitennummer_Erste_Seite();
+            Status.Instanz.AktuelleSeitenummer.Setze(seitenNummer);
+
+            List<CsvDatensatz> datensaetzeErsteSeite = _seitenBereitsteller.Filtere_Seite(alleDatensaetze, seitenlaenge, seitenNummer);
+
+            return datensaetzeErsteSeite;
         }
 
         public List<CsvDatensatz> Letzte_Seite()
         {
-            throw new NotImplementedException();
+            List<CsvDatensatz> alleDatensaetze = Status.Instanz.CsvDatensaetze.Lade();
+            int seitenlaenge = Status.Instanz.Seitenlaenge.Lade();
+            int seitenNummer = _seitenBereitsteller.Ermittle_Seitennummer_Letzte_Seite(alleDatensaetze, seitenlaenge);
+            Status.Instanz.AktuelleSeitenummer.Setze(seitenNummer);
+
+            List<CsvDatensatz> datensaetzeErsteSeite = _seitenBereitsteller.Filtere_Seite(alleDatensaetze, seitenlaenge, seitenNummer);
+
+            return datensaetzeErsteSeite;
         }
 
         public List<CsvDatensatz> Naechste_Seite()
         {
-            throw new NotImplementedException();
+            List<CsvDatensatz> alleDatensaetze = Status.Instanz.CsvDatensaetze.Lade();
+            int seitenlaenge = Status.Instanz.Seitenlaenge.Lade();
+            int aktuelleSeitennummer = Status.Instanz.AktuelleSeitenummer.Lade();
+            int seitenNummer = _seitenBereitsteller.Erhoehe_Seitennummer_um_eins(aktuelleSeitennummer);
+            Status.Instanz.AktuelleSeitenummer.Setze(seitenNummer);
+
+            List<CsvDatensatz> datensaetzeErsteSeite = _seitenBereitsteller.Filtere_Seite(alleDatensaetze, seitenlaenge, seitenNummer);
+
+            return datensaetzeErsteSeite;
         }
 
         public List<CsvDatensatz> Vorherige_Seite()
         {
-            throw new NotImplementedException();
+            List<CsvDatensatz> alleDatensaetze = Status.Instanz.CsvDatensaetze.Lade();
+            int seitenlaenge = Status.Instanz.Seitenlaenge.Lade();
+            int aktuelleSeitennummer = Status.Instanz.AktuelleSeitenummer.Lade();
+            int seitenNummer = _seitenBereitsteller.Verringere_Seitennummer_um_eins(aktuelleSeitennummer);
+            Status.Instanz.AktuelleSeitenummer.Setze(seitenNummer);
+
+            List<CsvDatensatz> datensaetzeErsteSeite = _seitenBereitsteller.Filtere_Seite(alleDatensaetze, seitenlaenge, seitenNummer);
+
+            return datensaetzeErsteSeite;
         }
     }
 }
--- Alex/CsvViewer/CsvViewer.Tests/InteraktionenTests.cs
+++ Alex/CsvViewer/CsvViewer.Tests/InteraktionenTests.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
 namespace CsvViewer.Tests
 {
+    [TestFixture]
     public class InteraktionenTests
     {
         public InteraktionenTests()
