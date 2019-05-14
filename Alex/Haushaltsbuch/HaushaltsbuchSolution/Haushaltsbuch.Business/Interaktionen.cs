7c55aa78a8dc14564b4431523fcdd1ee94436c8d
Alexander Badewitz <a.badewitz@adb-solutions.de>
2019-05-14 08:57:46 +0200

    ...

--- Alex/Haushaltsbuch/HaushaltsbuchSolution/Haushaltsbuch.Business/Interaktionen.cs
+++ Alex/Haushaltsbuch/HaushaltsbuchSolution/Haushaltsbuch.Business/Interaktionen.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Haushaltsbuch.Shared;
+using Money;
+
 namespace Haushaltsbuch.Business
 {
     public class Interaktionen
     {
-        public Interaktionen()
+        public void Start(string[] args, Action<Tuple<Money<decimal>, Kategorie>> onEinAuszahlung, Action<KategorieUebersicht> onUebersicht)
         {
+
         }
+
+
     }
 }
---
+++ Alex/Haushaltsbuch/HaushaltsbuchSolution/Haushaltsbuch.Shared/KategorieUebersicht.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Money;
+
+namespace Haushaltsbuch.Shared
+{
+    public class KategorieUebersicht
+    {
+        public DateTime Datum { get; set; }
+
+        public Money<decimal> Kassenbestand { get; set; }
+
+        public List<Kategorie> Kategorien { get; set; }
+    }
+}
