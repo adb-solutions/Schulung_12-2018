3fd2c4deeecdb94135738e6b56d9c466f0a5bfc0
Thiemann, Tom <t.thiemann@adb-solutions.de>
2019-05-15 18:15:50 +0200

    Struktur fast fertiggestellt. Funktionalität fehlt. Tests fehlen.

---
+++ Tom/Haushaltsbuch/Haushaltsbuch_mit_FlowDesign/FlowDesign/Business/Rechner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlowDesign.Shared;
+
+namespace FlowDesign.Business
+{
+    class Rechner
+    {
+        internal static Kategorie Ermittle_Kategore(DateTime zahlungsdatum, string kategorie, List<Transaktion> alleTransaktionen)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static decimal Kassenbestand_ermitteln(List<Transaktion> alleTransaktionen)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Kategorie Ermittle_alle_Kategorien(DateTime datum, List<Transaktion> alleTransaktionen)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static decimal Ermittle_Kassenbestand_der_Kategorie()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tom/Haushaltsbuch/Haushaltsbuch_mit_FlowDesign/FlowDesign/Persistenz/TransaktionsRespository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlowDesign.Shared;
+
+namespace FlowDesign.Persistenz
+{
+    public class TransaktionsRespository
+    {
+        internal void Speichern()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Transaktion> Lade_alle_Transaktionen()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Transaktion> Lade_alle_Transaktionen_vor_und_aus_dem_Zeitraum(DateTime datum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
